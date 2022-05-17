12867d367257e64816a987d0edd53d4a9e519dc9
Gerald Versluis <gerald.versluis@microsoft.com>
2022-05-17 20:57:49 +0200

    Initial commit

---
+++ MauiAndroidClearText/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiAndroidClearText;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# MauiAndroidClearText
+ Sample code to demonstrate how to enable Android Cleartext Traffic in .NET MAUI
